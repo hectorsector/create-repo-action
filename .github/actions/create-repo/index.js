0c5372d0295574e91ae6bd69430c355389138c82
Hector Alfaro <hectorsector@github.com>
2021-04-29 10:07:48 -0400

    refactor

--- .github/actions/create-repo/index.js
+++ .github/actions/create-repo/index.js
@@ -2,14 +2,21 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 const token = core.getInput("github-token")
+const deploy_key = core.getInput("repo-deploy-key")
+const target_owner = core.getInput("repo-owner")
+const target_repo = core.getInput("repo-name")
+const targetOctokit = core.getOctokit(deploy_key)
 const octokit = github.getOctokit(token)
 
 async function run() {
   try {
-    await octokit.repos.createForAuthenticatedUser({
-      name: 'hectors-test'
-    })
-  } catch (error) {
+    await targetOctokit.git.updateRef({
+      owner: target_owner,
+      repo: target_repo,
+      ref: 'main',
+      sha: github.context.sha,
+      force: true
+    })} catch (error) {
     core.setFailed(error.message)
   }  
 }
